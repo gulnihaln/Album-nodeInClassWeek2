b353bf752cc5f225bbb5d975e204ecd2234fe1ee
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-13 21:55:09 +0000

    feat: added albumsData and read

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const albumsData = require("./album.json");
 
 app.use(express.json());
+
+app.get("/albums", function (request, response) {
+  response.send(albumsData);
+  console.log("Request to get all albums");
+});
 
 app.listen(PORT, function () {
   console.log(`Server is listening on port ${PORT}. Ready to accept requests!`);
